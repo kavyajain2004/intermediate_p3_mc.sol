4a3ff724b7a6f4f601d1b89b9553b4f4d06c8538
kavyajain2004 <115156805+kavyajain2004@users.noreply.github.com>
2024-03-14 13:12:18 +0530

    int_p3_mc.sol

--- int_p3_mc.sol
+++ int_p3_mc.sol
@@ -1,29 +1,39 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-contract Token is ERC20
-{
-    address public owner;
-    constructor(string memory name,string memory symbol) ERC20(name,symbol)
-    {
-        owner=msg.sender;
+
+contract MyToken is ERC20 {
+
+    address owner;
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {
+        owner = msg.sender;
+        // Mint initial supply to the contract owner
+        _mint(msg.sender, 1000 * 10 ** decimals());
     }
 
-    modifier onlyOwner
-    {
-        require(msg.sender==owner,"Only owner can do this");
+    modifier checkAmount(uint amount) {
+        require(amount > 0, "Transfer amount must be greater than 0");
         _;
     }
 
-    function mint(address _to,uint amount) public onlyOwner 
-    {
-        _mint(_to,amount);
+    modifier onlyOwner {
+        require(msg.sender == owner, "Not the Owner");
+        _;
     }
 
-    function burn(address _from, uint amount) public
-    {
-        _burn(_from, amount);
+    function mint(address to, uint amount) external onlyOwner {
+        // Allowing only the owner to mint new tokens
+        _mint(to, amount);
     }
 
+    function burn(uint amount) external checkAmount(amount) {
+        // Allowing any user to burn their own tokens
+        _burn(msg.sender, amount);
+    }
 
+    function transfer(address to, uint amount) public checkAmount(amount) override returns (bool) {
+        // Overriding transfer function
+        return super.transfer(to, amount);
+    }
 }
